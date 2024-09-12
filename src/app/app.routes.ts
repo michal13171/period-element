3df830e735748efa96b4d979e0a4f67343223dd2
michal <michal131711@gmail.com>
2024-09-12 10:50:26 +0200

    create 2 routes for page not found and prepare listing for sqrt records

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import {AppComponent} from "./app.component";
+import {PageNotFoundComponent} from "./features/layout/page-not-found/page-not-found.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./features/pages/listing-sqrt/listing-sqrt.component')
+          .then(module => module.ListingSqrtComponent)
+      }]
+  },
+  {path: '', redirectTo: '/', pathMatch: 'full'},
+  {path: '**', component: PageNotFoundComponent}
+];
---
+++ src/app/features/pages/listing-sqrt/listing-sqrt.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listing-sqrt',
+  standalone: true,
+  imports: [],
+  templateUrl: './listing-sqrt.component.html',
+  styleUrl: './listing-sqrt.component.scss'
+})
+export class ListingSqrtComponent {
+
+}
