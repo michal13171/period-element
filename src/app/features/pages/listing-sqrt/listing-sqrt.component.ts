4fef222f29e742920b9978b9456133fbc367d12a
michal <michal131711@gmail.com>
2024-09-14 16:15:07 +0200

    get period data from file in cmp

--- src/app/features/pages/listing-sqrt/listing-sqrt.component.ts
+++ src/app/features/pages/listing-sqrt/listing-sqrt.component.ts
@@ -1,21 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {PeriodicElement} from "../../layout/models/PeriodicElement";
-import {BehaviorSubject, Observable} from "rxjs";
-import {DataSource} from "@angular/cdk/collections";
 import {CdkTableModule} from "@angular/cdk/table";
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+import {PeriodElementService} from "../../layout/services/period-element.service";
 
 @Component({
   selector: 'app-listing-sqrt',
@@ -24,19 +10,14 @@ const ELEMENT_DATA: PeriodicElement[] = [
   templateUrl: './listing-sqrt.component.html',
   styleUrl: './listing-sqrt.component.scss'
 })
-export class ListingSqrtComponent {
+export class ListingSqrtComponent implements OnInit {
+  private PeriodElementService = inject(PeriodElementService);
+
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new ExampleDataSource();
-}
+  dataSource: PeriodicElement[] = [];
 
-export class ExampleDataSource extends DataSource<PeriodicElement> {
-  /** Stream of data that is provided to the table. */
-  data = new BehaviorSubject<PeriodicElement[]>(ELEMENT_DATA);
-
-  /** Connect function called by the table to retrieve one stream containing the data to render. */
-  connect(): Observable<PeriodicElement[]> {
-    return this.data;
+  ngOnInit(): void {
+    this.PeriodElementService.getAllPeriodElements().subscribe(value => this.dataSource = value);
   }
 
-  disconnect() {}
 }
